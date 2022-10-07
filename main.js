93af6947844654f01d38935d81fc9616685b2c8e
l33ass <ilua-smirnof@yandex.ru>
2022-10-07 19:01:06 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+(function() {
+    const APP = {
+        init: function(e) {
+            console.log('init');
+        }
+    }
+    window.addEventListener("DOMContentLoaded", APP.init());
+})();
