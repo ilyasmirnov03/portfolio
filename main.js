e2bf615e2a4c848877eab016752090178cfea440
l33ass <ilua-smirnof@yandex.ru>
2022-10-24 11:34:48 +0200

    Content and details

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
 (function () {
-    /* ========
-    TODO: apply z-index to pages here
-    TODO: apply style display:none to all pages but the first here
-    ======= */
-    const PFAPP = {
+    const PTF = {
+
         allSections: document.querySelectorAll(".page"),
         currentSection: 0,
+        animationTime: parseInt(getComputedStyle(document.documentElement).getPropertyValue("--animation-time").charAt(1)) * 1000,
 
-        init: function (e) {
+        init: function () {
             console.log("init");
-            PFAPP.handlers();
+            PTF.zIndex();
+            PTF.dNone();
+            PTF.handlers();
+        },
+
+        // Applies z-index to every page in descendent order
+        zIndex: function () {
+            [...PTF.allSections].reverse().map((section, index) => {
+                section.style.zIndex = index + 1;
+                return section;
+            });
+        },
+
+        // Applies display:none to all pages but the first
+        dNone: function () {
+            for (let i = 1; i < PTF.allSections.length; i++) {
+                PTF.allSections[i].style.display = "none";
+            }
         },
 
         // On mouse move apply effect
         enableMouseMove: true,
         mouseMoveEffect: function (e) {
-            if (!PFAPP.enableMouseMove) return;
+            if (!PTF.enableMouseMove) return;
 
-            PFAPP.enableMouseMove = false;
+            PTF.enableMouseMove = false;
             let div = document.createElement("div");
             div.textContent = "+";
             div.classList.add("falling-x");
@@ -25,23 +40,23 @@
             div.style.top = e.y + "px";
             document.querySelector("body").appendChild(div);
             div.addEventListener("animationend", () => { div.remove(); });
-            setTimeout(() => { PFAPP.enableMouseMove = true }, 333);
+            setTimeout(() => { PTF.enableMouseMove = true }, 333);
         },
 
         // Scroll effect
         enableScroll: true,
         scroll: function (e) {
             // Easier to read
-            let as = PFAPP.allSections;
-            let cs = PFAPP.currentSection;
+            let as = PTF.allSections;
+            let cs = PTF.currentSection;
 
-            if (!PFAPP.enableScroll) return;
+            if (!PTF.enableScroll) return;
 
             // DOWN SCROLL
             if (e.deltaY === 100) {
                 if (cs === as.length - 1) return;
 
-                PFAPP.enableScroll = false;
+                PTF.enableScroll = false;
 
                 // goes forward hiding itself
                 as[cs].classList.add("go-forward");
@@ -60,16 +75,16 @@
                     as[cs].classList.remove("return-init-forward");
                     as[cs].classList.remove("go-backward");
 
-                    PFAPP.currentSection = cs;
-                    PFAPP.enableScroll = true
-                }, 1000);
+                    PTF.currentSection = cs;
+                    PTF.enableScroll = true
+                }, PTF.animationTime);
             }
 
             // UP SCROLL
             if (e.deltaY === -100) {
                 if (cs === 0) return;
 
-                PFAPP.enableScroll = false;
+                PTF.enableScroll = false;
 
                 // current section goes back
                 as[cs].classList.add("go-backward");
@@ -89,19 +104,19 @@
                     // remove in the current
                     as[cs].classList.remove("return-init-backward");
 
-                    PFAPP.currentSection = cs;
-                    PFAPP.enableScroll = true;
-                }, 1000);
+                    PTF.currentSection = cs;
+                    PTF.enableScroll = true;
+                }, PTF.animationTime);
             }
             // timeout with animation time
         },
 
         //Event listeners
         handlers: function (e) {
-            document.addEventListener("mousemove", PFAPP.mouseMoveEffect);
+            document.addEventListener("mousemove", PTF.mouseMoveEffect);
 
-            document.addEventListener("wheel", PFAPP.scroll);
+            document.addEventListener("wheel", PTF.scroll);
         }
     }
-    window.addEventListener("DOMContentLoaded", PFAPP.init);
+    window.addEventListener("DOMContentLoaded", PTF.init);
 })();
