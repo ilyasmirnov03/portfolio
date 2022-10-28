a61e6514f5841f39b5849142d7ec2fe18a5d06ee
l33ass <ilua-smirnof@yandex.ru>
2022-10-28 11:33:07 +0200

    Perfect navigation scroll

--- main.js
+++ main.js
@@ -6,9 +6,11 @@
         animationTime: parseInt(getComputedStyle(document.documentElement).getPropertyValue("--animation-time").charAt(1)) * 1000,
 
         init: function () {
-            console.log("init");
+            console.log("Hello World!");
+            // Dom manipulations
             PTF.zIndex();
             PTF.dNone();
+            // Ev listeners
             PTF.handlers();
         },
 
@@ -43,6 +45,70 @@
             setTimeout(() => { PTF.enableMouseMove = true }, 333);
         },
 
+        // t -> target, initially is one step
+        scrollUp: function (as, cs, t = 1) {
+            PTF.enableScroll = false;
+
+            // current section goes back
+            as[cs].classList.add("go-backward");
+            // change navbar state
+            document.querySelectorAll("#navigation>li")[cs].classList.remove("active-section");
+            // prev section goes to its place
+            as[cs - 1 * t].classList.add("return-init-backward");
+            as[cs - 1 * t].style.display = "block";
+
+            cs -= 1 * t;
+            // change navbar state
+            document.querySelectorAll("#navigation>li")[cs].classList.add("active-section");
+
+            // actions on prev section 
+            setTimeout(() => {
+                as[cs + 1 * t].style.display = "none";
+                //remove no more needed classes in prev section
+                as[cs + 1 * t].classList.remove("return-init-backward");
+                as[cs + 1 * t].classList.remove("go-forward");
+                as[cs + 1 * t].classList.remove("go-backward");
+
+                // remove in the current
+                as[cs].classList.remove("return-init-backward");
+
+                PTF.currentSection = cs;
+                PTF.enableScroll = true;
+            }, PTF.animationTime);
+        },
+
+        /* as -> all sections, cs -> current section, 
+        t -> target, if need to go through multiple sections,
+        n -> nullifier, to avoid getting out of number of sections */
+        scrollDown: function (as, cs, t = 1, n = 0) {
+            PTF.enableScroll = false;
+
+            // goes forward hiding itself
+            as[cs].classList.add("go-forward");
+            // change navbar state
+            document.querySelectorAll("#navigation>li")[cs].classList.remove("active-section");
+            // next section goes forward taking its place
+            as[cs + t - n].classList.add("return-init-forward");
+            as[cs + t - n].style.display = "block";
+
+            cs += t - n;
+            // change navbar state
+            document.querySelectorAll("#navigation>li")[cs].classList.add("active-section");
+
+            // actions on prev section 
+            setTimeout(() => {
+                as[cs - t + n].style.display = "none";
+                // remove no more needed classes in previous section
+                as[cs - t + n].classList.remove("go-forward");
+                //remove in the current
+                as[cs].classList.remove("return-init-forward");
+                as[cs].classList.remove("go-backward");
+
+                PTF.currentSection = cs;
+                PTF.enableScroll = true
+            }, PTF.animationTime);
+        },
+
         // Scroll effect
         enableScroll: true,
         scroll: function (e) {
@@ -55,67 +121,38 @@
 
             // DOWN SCROLL
             if (e.deltaY === 100 && cs !== as.length - 1) {
-
-                PTF.enableScroll = false;
-
-                // goes forward hiding itself
-                as[cs].classList.add("go-forward");
-                // next section goes forward taking its place
-                as[cs + 1].classList.add("return-init-forward")
-                as[cs + 1].style.display = "block";
-
-                cs += 1;
-
-                // actions on prev section 
-                setTimeout(() => {
-                    as[cs - 1].style.display = "none";
-                    // remove no more needed classes in previous section
-                    as[cs - 1].classList.remove("go-forward");
-                    //remove in the current
-                    as[cs].classList.remove("return-init-forward");
-                    as[cs].classList.remove("go-backward");
-
-                    PTF.currentSection = cs;
-                    PTF.enableScroll = true
-                }, PTF.animationTime);
+                PTF.scrollDown(as, cs);
             }
 
             // UP SCROLL
             if (e.deltaY === -100 && cs !== 0) {
-
-                PTF.enableScroll = false;
-
-                // current section goes back
-                as[cs].classList.add("go-backward");
-                // prev section goes to its place
-                as[cs - 1].classList.add("return-init-backward");
-                as[cs - 1].style.display = "block";
-
-                cs -= 1;
-
-                // actions on prev section 
-                setTimeout(() => {
-                    as[cs + 1].style.display = "none";
-                    //remove no more needed classes in prev section
-                    as[cs + 1].classList.remove("return-init-backward");
-                    as[cs + 1].classList.remove("go-forward");
-
-                    // remove in the current
-                    as[cs].classList.remove("return-init-backward");
-
-                    PTF.currentSection = cs;
-                    PTF.enableScroll = true;
-                }, PTF.animationTime);
+                PTF.scrollUp(as, cs);
             }
-            // timeout with animation time
+        },
+
+        scrollTo: function (e) {
+            e.preventDefault();
+            if (e.target.tagName === "A") {
+                let t = parseInt(e.target.closest("a").dataset.section);
+
+                if (t > PTF.currentSection) {
+                    PTF.scrollDown(PTF.allSections, PTF.currentSection, t, PTF.currentSection);
+                } else {
+                    PTF.scrollUp(PTF.allSections, PTF.currentSection, PTF.currentSection - t);
+                }
+                PTF.currentSection = parseInt(e.target.closest("a").dataset.section);
+            };
         },
 
         //Event listeners
         handlers: function (e) {
-            /* uncomm   ent when done */
+            /* uncomment when done */
             // document.addEventListener("mousemove", PTF.mouseMoveEffect);
 
             document.addEventListener("wheel", PTF.scroll);
+
+            document.querySelector("#navigation").addEventListener("click", PTF.scrollTo);
+            document.querySelector("#landing-btns").addEventListener("click", PTF.scrollTo);
         }
     }
     window.addEventListener("DOMContentLoaded", PTF.init);
