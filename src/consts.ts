fe8cc29c5535544b6913d61cb095c4154449b9a4
ilyasmirnov03 <ilua.smirnof@gmail.com>
2026-01-19 21:45:17 +0100

    feat(site): Customize title and description contents
    Customize title and description contents

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Ilya Smirnov - Backend, API & Full Stack engineer';
+export const SITE_DESCRIPTION = 'Ilya Smirnov is a Backend, API and Full Stack engineer and this is his blog about web development.';
